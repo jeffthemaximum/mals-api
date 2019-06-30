66a93395214241ad76e4dfe293102040c355198c
Jeff Maxim <frey.maxim@gmail.com>
2019-06-30 07:55:45 -0400

    adds user name to serializer

--- app/serializers/message_serializer.rb
+++ app/serializers/message_serializer.rb
@@ -2,7 +2,7 @@ class MessageSerializer < ActiveModel::Serializer
   attributes :id, :chat_id, :text, :user_id, :created_at, :user, :_id, :client_id
 
   def user
-    { _id: object.user_id }
+    { _id: object.user_id, name: object.user.name }
   end
 
   def _id
